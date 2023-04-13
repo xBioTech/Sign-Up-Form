311454921ca996052cd4374a338bac81f1882486
xbiotech <biotech9261@gmail.com>
2023-04-13 08:58:35 +0200

    Fix bug where the label of the input field transformed back when it was not valid this caused a bug, because the label moved in front of the users input

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const form = document.querySelector(".form");
 const scrollImage = document.querySelector(".scrollimg");
 const submitButton = document.querySelector(".submitbtn")
+const input = document.querySelectorAll("input");
 
 
 function scrollToForm(){
@@ -8,3 +9,15 @@ function scrollToForm(){
 }
 
 scrollImage.addEventListener("click", scrollToForm);
+
+
+input.forEach(input => {
+  input.addEventListener("input", () => {
+    const label = input.nextElementSibling;
+    if (input.value !== "") {
+      label.style.transform = "translateY(-20px)";
+    } else {
+      label.style.transform = "";
+    }
+  });
+});
